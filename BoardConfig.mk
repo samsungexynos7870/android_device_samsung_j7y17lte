75634c6ea3539141ab7db1e39e939925e93628ea
Astrako <apamarin@gmail.com>
2020-03-06 07:59:03 +0100

    j7y17lte: disable MAC randomization

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -55,6 +55,7 @@ WIFI_DRIVER_FW_PATH_STA          := "/vendor/etc/wifi/bcmdhd_sta.bin"
 WIFI_DRIVER_FW_PATH_AP           := "/vendor/etc/wifi/bcmdhd_apsta.bin"
 WIFI_BAND                        := 802_11_ABG
 WIFI_HIDL_FEATURE_DUAL_INTERFACE := true
+WIFI_HIDL_FEATURE_DISABLE_AP_MAC_RANDOMIZATION := true
 
 # inherit from common
 -include device/samsung/universal7870-common/BoardConfigCommon.mk
