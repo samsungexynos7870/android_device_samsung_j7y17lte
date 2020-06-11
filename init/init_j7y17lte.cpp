fd9fae036f1f37b50c247aa7b5d4f7b5c66f1330
Alejandro <apamarin@gmail.com>
2020-06-11 08:36:45 +0200

    j7y17lte: set S20 fingerprint for all variants

--- init/init_j7y17lte.cpp
+++ init/init_j7y17lte.cpp
@@ -65,33 +65,16 @@ void property_override_quad(const std::string& boot_prop, const std::string& pro
     property_override(vendor_prop, value);
 }
 
-void init_dsds() {
-    property_set("ro.multisim.set_audio_params", "true");
-    property_set("ro.multisim.simslotcount", "2");
-    property_set("persist.radio.multisim.config", "dsds");
-}
-
 void vendor_load_properties()
 {
     // Init a dummy BT MAC address, will be overwritten later
     property_set("ro.boot.btmacaddr", "00:00:00:00:00:00");
 
-    std::string bootloader = GetProperty("ro.bootloader","");
-
-    if (bootloader.find("J730F") == 0) {
-        /* SM-A320FL */
-        property_override_quad("ro.bootimage.build.fingerprint", "ro.build.fingerprint", "ro.odm.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/j7y17ltexc/j7y17lte:9.0.0/R16NW/J730FXXU6CSK6:user/release-keys");
-        property_override("ro.build.description", "j7y17ltexc-user 9.0.0 R16NW J730FXXU6CSK6 test-keys");
-        property_override_quad("ro.product.model", "ro.product.odm.model", "ro.product.system.model", "ro.product.vendor.model", "SM-J730F");
-        property_override_quad("ro.product.device", "ro.product.odm.device", "ro.product.system.device", "ro.product.vendor.device", "j7y17lte");
-        property_override_quad("ro.product.name", "ro.product.odm.name", "ro.product.system.name", "ro.product.vendor.name", "j7y17ltexc");
-
-        init_dsds();
-
-    }
-
-    std::string device = GetProperty("ro.product.device", "");
-    LOG(ERROR) << "Found bootloader id %s setting build properties for %s device\n" << bootloader.c_str() << device.c_str();
+    property_override_quad("ro.bootimage.build.fingerprint", "ro.build.fingerprint", "ro.odm.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/x1sxxx/x1s:10/QP1A.190711.020/G981BXXU1ATCT:user/release-keys:");
+    property_override("ro.build.description", "x1sxxx-user 10 QP1A.190711.020 G981BXXU1ATCT release-keys:");
+    property_override_quad("ro.product.model", "ro.product.odm.model", "ro.product.system.model", "ro.product.vendor.model", "SM-G981B");
+    property_override_quad("ro.product.device", "ro.product.odm.device", "ro.product.system.device", "ro.product.vendor.device", "x1s");
+    property_override_quad("ro.product.name", "ro.product.odm.name", "ro.product.system.name", "ro.product.vendor.name", "x1sxxx");
 }
 
 
