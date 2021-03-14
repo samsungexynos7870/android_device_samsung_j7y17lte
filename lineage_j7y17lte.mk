56f27ceb31cf0bd7485379b39515358dd779d119
Brian Gotte <bngotte@gmail.com>
2021-03-14 18:33:33 -0300

    j7y17lte: Define PRODUCT_MODEL

--- lineage_j7y17lte.mk
+++ lineage_j7y17lte.mk
@@ -29,6 +29,7 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 PRODUCT_DEVICE := j7y17lte
 PRODUCT_NAME := lineage_j7y17lte
 PRODUCT_BRAND := samsung
+PRODUCT_MODEL := Galaxy J7 2017
 PRODUCT_MANUFACTURER := samsung
 
 TARGET_VENDOR := samsung
