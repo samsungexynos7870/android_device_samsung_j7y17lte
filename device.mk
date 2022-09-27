1cc37e4c8d07f6402d769704125e07d1993c65b8
Batuhan Türkoğlu <76158371+batuhantrkgl@users.noreply.github.com>
2022-09-27 17:47:41 +0300

    j7y17lte: add fingerprint permission to device speciffic

--- device.mk
+++ device.mk
@@ -57,6 +57,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.samsung
 
+PRODUCT_COPY_FILES += \  
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
+
 # NFC
 PRODUCT_PACKAGES += \
     libnfc-nci \
